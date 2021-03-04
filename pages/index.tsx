568bc33227a8ee99b74ca6fc89d52c27a7ce3b57
Daniel <danielolaviobr@gmail.com>
2021-03-04 09:52:38 -0300

    Incrising the header size

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-gray-50">
-      <h1 className="mb-8 text-xl font-bold">Renderizações no Next.js</h1>
+      <h1 className="mb-8 text-2xl font-bold">Renderizações no Next.js</h1>
       <div className="flex flex-col items-center">
         <Link href="/CSR">
           <a>
