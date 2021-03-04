c41c03ee7536b744594a96b8efd14e3bfced81fb
Daniel <danielolaviobr@gmail.com>
2021-03-04 09:49:04 -0300

    Header for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,26 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen bg-gray-50">
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-50">
+      <h1 className="mb-8 text-xl font-bold">Renderizações no Next.js</h1>
       <div className="flex flex-col items-center">
         <Link href="/CSR">
           <a>
-            <div className="px-6 py-1 mb-4 text-lg font-bold text-white bg-blue-500 rounded shadow">
+            <div className="px-6 py-1 mb-4 text-lg font-bold text-white bg-blue-500 rounded shadow hover:bg-blue-600">
               CSR
             </div>
           </a>
         </Link>
         <Link href="/SSR">
           <a>
-            <div className="px-6 py-1 mb-4 text-lg font-bold text-white bg-blue-500 rounded shadow">
+            <div className="px-6 py-1 mb-4 text-lg font-bold text-white bg-blue-500 rounded shadow hover:bg-blue-600">
               SSR
             </div>
           </a>
         </Link>
         <Link href="/SSG">
           <a>
-            <div className="px-6 py-1 mb-4 text-lg font-bold text-white bg-blue-500 rounded shadow">
+            <div className="px-6 py-1 mb-4 text-lg font-bold text-white bg-blue-500 rounded shadow hover:bg-blue-600">
               SSG
             </div>
           </a>
