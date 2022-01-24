0f26e87424dbdb47dcfc2b575a7ba3d06ed16392
Daniel Olavio <danielolaviobr@gmail.com>
2022-01-24 09:38:55 +0000

    favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from "next/head";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Next Redering</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
